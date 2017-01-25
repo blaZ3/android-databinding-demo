3cf2ebadb8e4c5f38bae60c2f7c70724f6f2c506
vivek <vivek@docsapp.in>
2017-01-25 22:18:50 +0530

    enabled databinding

--- app/src/main/java/yearofcode/com/databindingdemo/MainActivity.java
+++ app/src/main/java/yearofcode/com/databindingdemo/MainActivity.java
@@ -1,13 +1,20 @@
 package yearofcode.com.databindingdemo;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import yearofcode.com.databindingdemo.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding dataBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        dataBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        dataBinding.setGreeting("Hello World!");
     }
 }
